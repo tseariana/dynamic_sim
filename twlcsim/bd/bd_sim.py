d4998ad001b3d0a38ba46abff4c9f7b6d71c1854
Ariana Tse <aytse@stanford.edu>
2023-03-23 13:29:32 -0700

    Modularize bd_sim code into collection of new functions

--- twlcsim/bd/bd_sim.py
+++ twlcsim/bd/bd_sim.py
@@ -23,83 +23,40 @@ def bd_sim(r_poly, t1_poly, t2_poly, t3_poly, twist_poly, num_polymers, num_bead
     sim_type, eps_bend, eps_par, eps_perp, eps_twist, gamma, eta, \
         xi_r_drag_coef, xi_t3_drag_coef, xi_t12_drag_coef, dt_bd = find_parameters(length, b, num_beads)
 
-    #mag_force_brown = np.sqrt(2 * xi_r_drag_coef / dt_bd)       # Prefactor for Brownian forces
-    #mag_torque_12_brown = np.sqrt(2 * xi_t12_drag_coef / dt_bd)      # Prefactor for Brownian torques
-    #mag_torque_3_brown = np.sqrt(2 * xi_t3_drag_coef / dt_bd)      # Prefactor for Brownian torques
-    ##mag_active_brown= np.sqrt((2 * force_active0**2 * k_a) / dt_bd)    # (3* dt_bd)??? Prefactor for active forces
-
     while time < time_total:
-        dt_bd = 0.005
-        if confinement_tag:
-            r = np.sqrt(np.sum(r_poly ** 2, axis=1))
-            dis_fr_conf = r - a
-            max= (1/(6*epsilon*dt_bd))**(1/10)
-            if np.any(dis_fr_conf > max): # if any distance  is postive
-                dis = np.max(dis_fr_conf[dis_fr_conf>max])
-                dis_force_boundary = -6 * epsilon * dis ** 11
-                dt_bd = (xi_r_drag_coef * dis / np.absolute(dis_force_boundary)) * 0.2
-                # dt_bd= dt_bd * 10/((max_force)**.91)
-            else:
-                pass
-            force_boundary = calc_boundary_force(r_poly, num_polymers, num_beads, a, epsilon)
+        # Determine differential time
+        dt_bd = calc_differential_time(r_poly, confinement_tag, a, epsilon, xi_r_drag_coef)
 
+
+        # Calculate boundary force
+        if confinement_tag:
+            force_boundary = calc_boundary_force(r_poly, num_polymers, num_beads, a, epsilon)
         else:
             force_boundary = np.zeros(r_poly.shape)
 
-        mag_force_brown = np.sqrt(2 * xi_r_drag_coef / dt_bd)       # Prefactor for Brownian forces
-        mag_torque_12_brown = np.sqrt(2 * xi_t12_drag_coef / dt_bd)      # Prefactor for Brownian torques
-        mag_torque_3_brown = np.sqrt(2 * xi_t3_drag_coef / dt_bd)      # Prefactor for Brownian torques
-        #mag_active_brown= np.sqrt((2 * force_active0**2 * k_a) / dt_bd)    # (3* dt_bd)??? Prefactor for active forces
 
-        dt_bd_a = dt_bd * k_a
-        mag_active = np.sqrt(2 * force_active0 ** 2 * k_a)
-        w0_2 = 1 / (2 * k_a) * (1 - np.exp(-2 * dt_bd_a))
-        w1_2 = 1 / (2 * k_a ** 3) * (2 * dt_bd_a - 3 - np.exp(-2 * dt_bd_a) + 4 * np.exp(-dt_bd_a))
-        w0w1 = 1 / (2 * k_a ** 2) * (1 - 2 * np.exp(-dt_bd_a) + np.exp(-2 * dt_bd_a))
-
-        # forces and torques on the beads
-        force_brown = mag_force_brown * np.random.randn(num_total, 3)  # Brownian forces
-        torque_1_brown = mag_torque_12_brown * np.random.randn(num_total, 3)  # Brownian torques
-        torque_2_brown = mag_torque_12_brown * np.random.randn(num_total, 3)  # Brownian torques
-        torque_3_brown = mag_torque_3_brown * np.random.randn(num_total, 3)  # Brownian torques
-
-        force_pot, torque_1_pot, torque_2_pot, torque_3_pot = calc_force(
-            r_poly, t1_poly, t2_poly, t3_poly, twist_poly,
-            num_polymers, num_beads, sim_type,
-            eps_bend, eps_par, eps_perp, eps_twist,
-            gamma, eta) # Potential force and torque
+        # Calculate brownian force
+        force_brown, torque_1_brown, torque_2_brown, torque_3_brown = calc_brownian_force(num_total, xi_r_drag_coef, xi_t12_drag_coef, xi_t3_drag_coef, dt_bd)
 
 
-        # active force calculation
-        w0 = np.sqrt(w0_2) * np.random.randn(num_total, 3)
-        w1 = w0w1 / np.sqrt(w0_2) * np.random.randn(num_total, 3) + np.sqrt(np.absolute(w1_2 - (w0w1 ** 2 / w0_2))) * np.random.randn(num_total, 3)
+        # Calculate active force
+        dt_bd_a, mag_active, w0, active_force_r_integration= calc_temp_corr_active_force(num_total, force_active0, k_a, dt_bd, force_active, fl_only_tag, fl_indices)
 
-        active_force_r_integration = (1 / k_a * (1 - np.exp(-dt_bd_a)) * force_active + mag_active * w1)
 
-        if fl_only_tag:
-            active_force_r_integration_0= np.zeros(active_force_r_integration.shape)
-            active_force_r_integration_0[fl_indices,:] = active_force_r_integration[fl_indices,:]
-            active_force_r_integration= active_force_r_integration_0
-        else:
-            pass
+        # Calculate internal force
+        force_pot, torque_1_pot, torque_2_pot, torque_3_pot = calc_force(r_poly, t1_poly, t2_poly, t3_poly, twist_poly, num_polymers, num_beads, sim_type,
+                                                                         eps_bend, eps_par, eps_perp, eps_twist,
+                                                                         gamma, eta)
+
 
         # rate of change of the position and orientation variables
         drdt_poly = (force_brown + force_pot + force_boundary) / xi_r_drag_coef
-        dt1dt_poly = (
-            (np.sum((torque_1_pot + torque_1_brown) * t2_poly, axis=1)
-             - np.sum((torque_2_pot + torque_2_brown) * t1_poly, axis=1))[:, None] *
-            t2_poly / (2 * xi_t12_drag_coef)) + (
-            (np.sum((torque_1_pot + torque_1_brown) * t3_poly, axis=1)
-             - np.sum((torque_3_pot + torque_3_brown) * t1_poly, axis=1))[:, None] *
-            t3_poly / (xi_t12_drag_coef + xi_t3_drag_coef))
-        dt3dt_poly = (
-            (np.sum((torque_3_pot + torque_3_brown) * t1_poly,
-                    axis=1)
-             - np.sum((torque_1_pot + torque_1_brown) * t3_poly, axis=1))[:, None] *
-            t1_poly / (xi_t12_drag_coef + xi_t3_drag_coef)) + (
-            (np.sum((torque_3_pot + torque_3_brown) * t2_poly, axis=1)
-             - np.sum((torque_2_pot + torque_2_brown) * t3_poly, axis=1))[:, None] *
-            t2_poly / (xi_t12_drag_coef + xi_t3_drag_coef))
+
+        dt1dt_poly = ((np.sum((torque_1_pot + torque_1_brown) * t2_poly, axis=1) - np.sum((torque_2_pot + torque_2_brown) * t1_poly, axis=1))[:, None] * t2_poly / (2 * xi_t12_drag_coef)) + \
+                     ((np.sum((torque_1_pot + torque_1_brown) * t3_poly, axis=1) - np.sum((torque_3_pot + torque_3_brown) * t1_poly, axis=1))[:, None] * t3_poly / (xi_t12_drag_coef + xi_t3_drag_coef))
+
+        dt3dt_poly = ((np.sum((torque_3_pot + torque_3_brown) * t1_poly, axis=1) - np.sum((torque_1_pot + torque_1_brown) * t3_poly, axis=1))[:, None] * t1_poly / (xi_t12_drag_coef + xi_t3_drag_coef)) + \
+                     ((np.sum((torque_3_pot + torque_3_brown) * t2_poly, axis=1) - np.sum((torque_2_pot + torque_2_brown) * t3_poly, axis=1))[:, None] * t2_poly / (xi_t12_drag_coef + xi_t3_drag_coef))
 
 
         # Update the position and orientation variables
@@ -115,17 +72,9 @@ def bd_sim(r_poly, t1_poly, t2_poly, t3_poly, twist_poly, num_polymers, num_bead
         else:
             pass
 
-
         # Force pinning of first and last beads
         if confinement_tag:
-            fl_r_poly = r_poly[fl_indices, :]
-            r_fl_r_poly= np.sqrt(np.sum(fl_r_poly ** 2, axis=1))
-            ratio = a / r_fl_r_poly
-            ratio = np.transpose(np.tile(ratio, (3, 1)))
-            new_r_poly = fl_r_poly * ratio
-            r_poly[fl_indices, :] = new_r_poly
-        else:
-            pass
+            r_poly = pin_beads(r_poly, fl_indices, a)
 
         # Ensure the t vectors are orthonormal
         t1_poly, t2_poly, t3_poly = fix_t_vectors(t1_poly, t2_poly, t3_poly)
@@ -135,8 +84,121 @@ def bd_sim(r_poly, t1_poly, t2_poly, t3_poly, twist_poly, num_polymers, num_bead
 
         time += dt_bd  # Update the integration time at end of time step
 
-
     return r_poly, t1_poly, t2_poly, t3_poly, twist_poly, time, force_active #, force_boundary
+
+
+def calc_differential_time(r_poly, confinement_tag, a, epsilon, xi_r_drag_coef):
+    """
+    Calculates the differential time step. Necessary for confined chains to prevent differential instabilities as a result of large confinement forces.
+
+    :param r_poly: position vector of all beads in simulation (num_total,3)
+    :param confinement_tag: boolean-- whether polymers are confined in sphere
+    :param a: radius spherical confinement
+
+    :return: dt_bd: Differential time step
+    """
+    dt_bd = 0.005
+    if confinement_tag:
+        r = np.sqrt(np.sum(r_poly ** 2, axis=1))
+        dis_fr_conf = r - a
+        max= (1/(6*epsilon*dt_bd))**(1/10)
+        if np.any(dis_fr_conf > max): # if any distance  is postive
+            dis = np.max(dis_fr_conf[dis_fr_conf>max])
+            dis_force_boundary = -6 * epsilon * dis ** 11
+            dt_bd = (xi_r_drag_coef * dis / np.absolute(dis_force_boundary)) * 0.2
+        else:
+            pass
+
+    return dt_bd
+
+
+def calc_brownian_force(num_total, xi_r_drag_coef, xi_t12_drag_coef, xi_t3_drag_coef, dt_bd):
+    """
+    Calculate Brownian forces for each bead in simulation
+
+    :param num_total: Number of beads in simulation
+    :param xi_r_drag_coef: Drag coefficient - Position
+    :param xi_t12_drag_coef: Drag coefficient - Rotational
+    :param xi_t3_drag_coef: Drag coefficient - Rotational
+    :param dt_bd: Differential time step
+
+    :return: force_brown : Brownian force (num_total, 3)
+    :return: torque_1_brown : Brownian force - Rotational (num_total, 3)
+    :return: torque_2_brown : Brownian force - Rotational (num_total, 3)
+    :return: torque_3_brown : Brownian force - Rotational (num_total, 3)
+    """
+    mag_force_brown = np.sqrt(2 * xi_r_drag_coef / dt_bd)       # Prefactor for Brownian forces
+    mag_torque_12_brown = np.sqrt(2 * xi_t12_drag_coef / dt_bd)      # Prefactor for Brownian torques
+    mag_torque_3_brown = np.sqrt(2 * xi_t3_drag_coef / dt_bd)      # Prefactor for Brownian torques
+
+    # forces and torques on the beads
+    force_brown = mag_force_brown * np.random.randn(num_total, 3)  # Brownian forces
+    torque_1_brown = mag_torque_12_brown * np.random.randn(num_total, 3)  # Brownian torques
+    torque_2_brown = mag_torque_12_brown * np.random.randn(num_total, 3)  # Brownian torques
+    torque_3_brown = mag_torque_3_brown * np.random.randn(num_total, 3)  # Brownian torques
+
+    return force_brown, torque_1_brown, torque_2_brown, torque_3_brown
+
+
+def calc_temp_corr_active_force(num_total, force_active0, k_a, dt_bd, force_active, fl_only_tag, fl_indices):
+    """
+    Calculate temporally-correlated active force based off the theory of Ghosh et al (2022).
+
+    :param num_total: Number of beads in simulation
+    :param force_active0: Active force scale
+    :param k_a: Active force relaxation rate
+    :param dt_bd: Differential time step
+    :param force_active: Previously applied active force
+    :param fl_only_tag: boolean -- whether active force is only applied on first and last beads of chains
+    :param fl_indices: Indices of first and last beads of chain
+
+    :return: dt_bd_a: Differential time step for active force
+    :return: mag_active: Active force magnitude
+    :return: w0: variable required for proper integration of active force
+    :return: active_force_r_integration: Active force integration
+    """
+    dt_bd_a = dt_bd * k_a
+    mag_active = np.sqrt(2 * force_active0 ** 2 * k_a)
+
+    w0_2 = 1 / (2 * k_a) * (1 - np.exp(-2 * dt_bd_a))
+    w1_2 = 1 / (2 * k_a ** 3) * (2 * dt_bd_a - 3 - np.exp(-2 * dt_bd_a) + 4 * np.exp(-dt_bd_a))
+    w0w1 = 1 / (2 * k_a ** 2) * (1 - 2 * np.exp(-dt_bd_a) + np.exp(-2 * dt_bd_a))
+
+    # active force calculation
+    w0 = np.sqrt(w0_2) * np.random.randn(num_total, 3)
+    w1 = w0w1 / np.sqrt(w0_2) * np.random.randn(num_total, 3) + np.sqrt(np.absolute(w1_2 - (w0w1 ** 2 / w0_2))) * np.random.randn(num_total, 3)
+
+    active_force_r_integration = (1 / k_a * (1 - np.exp(-dt_bd_a)) * force_active + mag_active * w1)
+
+    if fl_only_tag:
+        active_force_r_integration_0= np.zeros(active_force_r_integration.shape)
+        active_force_r_integration_0[fl_indices,:] = active_force_r_integration[fl_indices,:]
+        active_force_r_integration= active_force_r_integration_0
+    else:
+        pass
+
+    return dt_bd_a, mag_active, w0, active_force_r_integration
+
+def pin_beads(r_poly, fl_indices, a):
+    """
+    Returns r_poly but with first and last beads of chains pinned to spherical confinement
+
+    :param r_poly: Positional vector of all beads in simulation (num_total, 3)
+    :param fl_indices: Indices of first and last beads of polymers
+    :param a: Radius of spherical confinement
+
+    :return: r_poly_pinned: Positional vector of chain with first and last beads pinned to the spherical confinement
+    """
+    r_poly_pinned = np.copy(r_poly)
+    fl_r_poly = r_poly_pinned[fl_indices, :]
+    radial_dis_fl = np.sqrt(np.sum(fl_r_poly ** 2, axis=1))
+    ratio = a / radial_dis_fl
+    ratio = np.transpose(np.tile(ratio, (3, 1)))
+    new_r_poly = fl_r_poly * ratio
+    r_poly_pinned[fl_indices, :] = new_r_poly
+
+    return r_poly_pinned
+
 
 
 def convert_t_to_twist(t1_poly, t2_poly, twist_poly, num_beads, num_polymers):
@@ -148,6 +210,7 @@ def convert_t_to_twist(t1_poly, t2_poly, twist_poly, num_beads, num_polymers):
     :param twist_poly: Current value of the twist angle
     :param num_beads: Number of beads in each chain
     :param num_polymers: Number of polymer chains
+
     :return: twist_poly: Updated twist angle from t1 and t2
     """
     for i_poly in range(num_polymers):
